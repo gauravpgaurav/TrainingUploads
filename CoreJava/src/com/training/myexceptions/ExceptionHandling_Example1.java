171d4770b361ed94663a08c0c5c9c9968c439829
Gaurav Pant <gauravpgaurav@gmail.com>
2016-01-29 15:58:44 +0530

    NullPointer

--- CoreJava/src/com/training/myexceptions/ExceptionHandling_Example1.java
+++ CoreJava/src/com/training/myexceptions/ExceptionHandling_Example1.java
@@ -4,13 +4,19 @@ public class ExceptionHandling_Example1 {
 	
 	public static void main(String[] args){
 		
+		String name=null;
 		
 		try{
 			System.out.println(args[0]);
+			System.out.println(name.length());
 		}
 		catch(ArrayIndexOutOfBoundsException e)
 		{
 			System.out.println("Enter Command Line Argument via Run Config-To !");
+		}
+		catch(NullPointerException e)
+		{
+			System.out.println("Check Attribute-Name It mayve Null !");
 		}
 	}
 
