5e12db8f3610016c48f9e78ec55e4b9ab8b66f84
Gaurav Pant <gauravpgaurav@gmail.com>
2016-02-02 11:16:56 +0530

    123

--- ExceptionHandling/src/com/training/Converter.java
+++ ExceptionHandling/src/com/training/Converter.java
@@ -4,8 +4,15 @@ public class Converter {
 	
 	public double farenToCelsius(Double faren){
 		
-		double cel = ((5.0/9.0)*(faren-32.0));
-		
+		double cel = 0.0;
+		try{
+		cel = ((5.0/9.0)*(faren-32.0));
+		}
+		catch(NullPointerException e)
+		{
+			System.out.println("Farenheit should not be null !");
+			System.out.println(e.getClass());
+		}
 		return cel;
 	}
 	
