4e5647b2d5ce3776fdbfc0001a65d4dcbfd9d4b2
Gaurav Pant <gauravpgaurav@gmail.com>
2016-02-02 17:00:25 +0530

    Synchronized

---
+++ ThreadsExample/src/com/training/UsePrintString.java
@@ -0,0 +1,29 @@
+package com.training;
+
+public class UsePrintString implements Runnable {
+
+	private String str1,str2;
+	
+	
+	
+	public UsePrintString(String str1, String str2) {
+		super();
+		this.str1 = str1;
+		this.str2 = str2;
+		
+		Thread t = new Thread(this);
+		// this refers to current instance which is Runnable 
+		
+		t.start();
+		
+	}
+
+
+
+	@Override
+	public void run() {
+
+		PrintString.print(str1, str2);
+	}
+
+}
