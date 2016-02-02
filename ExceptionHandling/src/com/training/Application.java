c8590e7d66985d980d74a3cf420725acea07850f
Gaurav Pant <gauravpgaurav@gmail.com>
2016-02-02 11:26:22 +0530

    12

--- ExceptionHandling/src/com/training/Application.java
+++ ExceptionHandling/src/com/training/Application.java
@@ -6,13 +6,20 @@ public class Application {
 		
 		Converter obj = new Converter();
 		double cel = obj.farenToCelsius(45.00);
-		
+		double cel2 = 0.0;
 		System.out.println("Celsius value of 45F : "+cel);
 		
 		Converter obj2 = new Converter();
 		Double far2 = null;
-		double cel2 = obj.farenToCelsius(far2);
 		
+		try{
+		 cel2 = obj.farenToCelsius(far2);
+		}
+		catch(NullPointerException e){
+			
+			System.out.println("Faren is NULL !");
+			System.out.println(e.getMessage());
+		}
 		
 		System.out.println("Celsius value of null : "+cel2);
 	}
--- ExceptionHandling/src/com/training/Converter.java
+++ ExceptionHandling/src/com/training/Converter.java
@@ -2,17 +2,17 @@ package com.training;
 
 public class Converter {
 	
-	public double farenToCelsius(Double faren){
+	public double farenToCelsius(Double faren) throws NullPointerException{
 		
 		double cel = 0.0;
-		try{
+//		try{
 		cel = ((5.0/9.0)*(faren-32.0));
-		}
-		catch(NullPointerException e)
-		{
-			System.out.println("Farenheit should not be null !");
-			System.out.println(e.getClass());
-		}
+//		}
+//		catch(NullPointerException e)
+//		{
+//			System.out.println("Farenheit should not be null !");
+//			System.out.println(e.getClass());
+//		}
 		return cel;
 	}
 	
