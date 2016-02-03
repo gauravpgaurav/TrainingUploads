485f369df4af22e9a9b56269c6e324f6ec1cd981
Gaurav Pant <gauravpgaurav@gmail.com>
2016-02-03 15:09:24 +0530

    File_Example Added

---
+++ ProblemStatement/src/com/training/File_Example.java
@@ -0,0 +1,38 @@
+package com.training;
+
+import java.io.*;
+
+public class File_Example {
+
+	public static void main(String[] args) {
+		
+			File f = new File("Sample.txt");
+			
+			try 
+			{
+				f.createNewFile();
+			}
+			
+			catch (IOException e) 
+			{
+				System.out.println(e.getMessage());
+			}
+			
+			PrintWriter writer = null;
+			
+			try
+			{
+				writer = new PrintWriter(f);
+				writer.print("Hello");
+				writer.close();
+			}
+			
+			catch (FileNotFoundException e) 
+			{
+				System.out.println(e.getMessage());
+			}
+			
+			System.out.println(f.exists());
+			
+	}
+}
