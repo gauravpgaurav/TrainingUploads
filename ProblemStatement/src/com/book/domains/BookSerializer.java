e3a9f21222c8026c4dd26b17675d3c49a254c775
Gaurav Pant <gauravpgaurav@gmail.com>
2016-02-03 11:07:50 +0530

    Serial

--- ProblemStatement/src/com/book/domains/BookSerializer.java
+++ ProblemStatement/src/com/book/domains/BookSerializer.java
@@ -1,12 +1,37 @@
 package com.book.domains;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 import com.book.Book;
 
 public class BookSerializer {
 
 	public String serialize(Book obj){
 		
+		ObjectOutputStream out =null;
 		String message = null;
+		
+		try {
+			 out = new ObjectOutputStream(new FileOutputStream(new File("Book.ser")));
+		} 
+
+		catch (IOException e) {
+
+				e.getMessage();
+		}
+		finally{
+			try {
+				out.close();
+			} 
+			catch (IOException e) {
+
+				e.getMessage();
+			}
+		}
 		return message;
 	}
 	
