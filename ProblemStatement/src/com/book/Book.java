2ddea4f9f0126cd88677136aeb316cba539ab2be
Gaurav Pant <gauravpgaurav@gmail.com>
2016-02-03 11:02:09 +0530

    Serialize

---
+++ ProblemStatement/src/com/book/Book.java
@@ -0,0 +1,60 @@
+package com.book;
+
+import java.io.Serializable;
+
+public class Book implements Serializable{
+	
+	private int bookNumber;
+	private String bookName;
+	private String author;
+	private transient double price;
+	
+	
+	public Book() {
+		
+		super();
+		System.out.println("Book Initialized");
+	}
+	
+	public Book(int bookNumber, String bookName, String author, double price) {
+		super();
+		this.bookNumber = bookNumber;
+		this.bookName = bookName;
+		this.author = author;
+		this.price = price;
+	}
+
+	public int getBookNumber() {
+		return bookNumber;
+	}
+	public void setBookNumber(int bookNumber) {
+		this.bookNumber = bookNumber;
+	}
+	public String getBookName() {
+		return bookName;
+	}
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	@Override
+	public String toString() {
+		
+		return bookNumber + " : " + bookNumber + " : " + author ;
+	}
+	
+	
+
+	
+}
---
+++ ProblemStatement/src/com/book/domains/BookSerializer.java
@@ -0,0 +1,19 @@
+package com.book.domains;
+
+import com.book.Book;
+
+public class BookSerializer {
+
+	public String serialize(Book obj){
+		
+		String message = null;
+		return message;
+	}
+	
+	public Object deSerialize(){
+		
+		Object obj = null;
+		return obj;
+		
+	}
+}
